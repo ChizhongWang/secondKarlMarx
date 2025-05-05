6f3009f8e37f385f83805a4f3a617ebbf80319bc
ChizhongWang <chizhongwang1128@gmail.com>
2025-05-05 17:55:51 +0800

    修复LLaMA Factory API命令行参数格式

--- run_api_server.sh
+++ run_api_server.sh
@@ -7,6 +7,6 @@ PORT=${1:-8000}
 echo "启动API服务器，端口: $PORT..."
 
 # 启动API服务器
-llamafactory-cli api qwen_inference.yaml --port $PORT --host 0.0.0.0
+llamafactory-cli api qwen_inference.yaml --port=$PORT --host=0.0.0.0
 
 echo "API服务器已启动，可以通过 http://[服务器IP]:$PORT 访问"
