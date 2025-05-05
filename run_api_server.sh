ed745ee0818df6ae96e6770989d86dbc9b745529
ChizhongWang <chizhongwang1128@gmail.com>
2025-05-05 18:39:50 +0800

    修改LLaMA Factory API配置，将参数移到配置文件中

--- run_api_server.sh
+++ run_api_server.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # 启动LLaMA Factory API服务器
 
-# 设置端口，默认为8000
-PORT=${1:-8000}
-
-echo "启动API服务器，端口: $PORT..."
+echo "启动API服务器..."
 
 # 启动API服务器
-llamafactory-cli api qwen_inference.yaml --port=$PORT --host=0.0.0.0
+llamafactory-cli api qwen_inference.yaml
 
-echo "API服务器已启动，可以通过 http://[服务器IP]:$PORT 访问"
+echo "API服务器已启动，可以通过 http://[服务器IP]:8000 访问"
