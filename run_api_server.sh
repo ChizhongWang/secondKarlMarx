5a646d4ec8f49de7301a24b60b92116a204c2755
ChizhongWang <chizhongwang1128@gmail.com>
2025-05-05 18:53:30 +0800

    修改LLaMA Factory API服务器启动脚本，使用命令行环境变量指定端口

--- run_api_server.sh
+++ run_api_server.sh
@@ -3,11 +3,7 @@
 
 echo "启动API服务器..."
 
-# 设置环境变量
-export API_HOST=0.0.0.0
-export API_PORT=8000
-
-# 启动API服务器
-llamafactory-cli api qwen_inference.yaml
+# 启动API服务器（使用环境变量指定端口）
+API_PORT=8000 llamafactory-cli api qwen_inference.yaml
 
 echo "API服务器已启动，可以通过 http://[服务器IP]:8000 访问"
